27f4de6d74e75e14efe38055ad1521278242914d
thmeme <thierrymeme@gmail.com>
2017-03-06 11:47:52 +0100

    modif index et studentsList.js

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,14 @@
-// Write yout Javascript code in this files
+var request = new XMLHttpRequest();
+
+request.open('GET', 'students.json', true);
+request.onreadystatechange = function () {
+  if (request.readyState == 4) {
+     if(request.status == 200) {
+       var student = JSON.parse(request.responseText);
+console.log(student);
+} else {
+      dump("Erreur pendant le chargement de la page.\n");
+  }
+}
+};
+request.send()
