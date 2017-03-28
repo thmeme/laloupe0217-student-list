105fe53810000dcae4e3a78b1da0da2a42d72e8e
thmeme <thierrymeme@gmail.com>
2017-03-28 16:01:49 +0200

    ok

--- studentsList.js
+++ studentsList.js
@@ -1,14 +1,25 @@
 var request = new XMLHttpRequest();
+var table = document.getElementById("students");
 
 request.open('GET', 'students.json', true);
-request.onreadystatechange = function () {
-  if (request.readyState == 4) {
-     if(request.status == 200) {
-       var student = JSON.parse(request.responseText);
-console.log(student);
-} else {
-      dump("Erreur pendant le chargement de la page.\n");
-  }
-}
+request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+        if (request.status == 200) {
+            var students = JSON.parse(request.responseText);
+
+            students.forEach(function (student) {
+              let tr = document.createElement('tr');
+              tr.innerHTML = `<td>${student.firstname}</td>
+                              <td>${student.lastname}</td>
+                              <td>${student['postal-code']}</td>`;
+              table.appendChild(tr);
+              console.log(student.firstname);
+
+            });
+
+        } else {
+            dump("Erreur pendant le chargement de la page.\n");
+        }
+    }
 };
-request.send()
+request.send();
